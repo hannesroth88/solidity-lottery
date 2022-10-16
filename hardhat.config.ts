be48b4b3259e12e1a4328b1293b6d32174a824ee
Roth Hannes (GS/BDA1) <Hannes.Roth2@de.bosch.com>
2022-10-16 02:01:20 +0200

    went to working version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,52 +1,90 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "hardhat-deploy";
-import "@nomicfoundation/hardhat-toolbox";
-import "hardhat-gas-reporter";
-import * as dotenv from "dotenv";
-dotenv.config();
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-waffle"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-gas-reporter"
+import "dotenv/config"
+import "solidity-coverage"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL as string;
-const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
-const COINMARKETCAP_APIKEY = process.env.COINMARKETCAP_APIKEY as string;
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string;
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+const MAINNET_RPC_URL =
+    process.env.MAINNET_RPC_URL ||
+    process.env.ALCHEMY_MAINNET_RPC_URL ||
+    "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL as string
+const PRIVATE_KEY = process.env.PRIVATE_KEY as string
+
+const COINMARKETCAP_APIKEY = process.env.COINMARKETCAP_APIKEY as string
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY as string
+const REPORT_GAS = process.env.REPORT_GAS || false
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-  defaultNetwork: "hardhat",
-  networks: {
-    hardhat: {
-      chainId: 31337,
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            // // If you want to do some forking, uncomment this
+            // forking: {
+            //   url: MAINNET_RPC_URL
+            // }
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        mainnet: {
+            url: MAINNET_RPC_URL,
+            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+            //   accounts: {
+            //     mnemonic: MNEMONIC,
+            //   },
+            saveDeployments: true,
+            chainId: 1,
+        },
     },
-    localhost: {
-        chainId: 31337,
+    gasReporter: {
+        currency: "EUR",
+        gasPrice: 21,
+        enabled: process.env.REPORT_GAS ? true : false,
+        coinmarketcap: COINMARKETCAP_APIKEY,
     },
-    goerli: {
-      url: GOERLI_RPC_URL,
-      accounts: [PRIVATE_KEY],
-      chainId: 5,
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
     },
-  },
-  gasReporter: {
-    currency: "EUR",
-    gasPrice: 21,
-    enabled: process.env.REPORT_GAS ? true : false,
-    coinmarketcap: COINMARKETCAP_APIKEY,
-  },
-  etherscan: {
-    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-    apiKey: {
-      goerli: ETHERSCAN_API_KEY,
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        player: {
+            default: 1,
+        },
     },
-  },
-  namedAccounts: {
-    deployer: {
-      default: 0, // here this will by default take the first account as deployer
-      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.8",
+            },
+            {
+                version: "0.4.24",
+            },
+        ],
     },
-    player: {
-      default: 1,
+    mocha: {
+        timeout: 200000, // 200 seconds max for running tests
     },
-  },
-};
+}
 
-export default config;
+export default config
